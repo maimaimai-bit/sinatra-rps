16d09db4e047e90dd15f0cd38f712967281bfef3
maimaimai-bit <hmaivu02@gmail.com>
2025-01-27 23:03:30 -0600

    commit

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ end
 get("/rock") do
   moves = ["rock", "paper", "scissors"]
   @computer_move = moves.sample
+  @player_move = "rock"
   
   if @computer_move == "rock"
     @outcome = "We tied"
@@ -23,6 +24,7 @@ end
 get("/paper") do
   moves = ["rock", "paper", "scissors"]
   @computer_move = moves.sample
+  @player_move = "paper"
   
   if @computer_move == "paper"
     @outcome = "We tied"
@@ -38,6 +40,7 @@ end
 get("/scissors") do
   moves = ["rock", "paper", "scissors"]
   @computer_move = moves.sample
+  @player_move = "scissors"
   
   if @computer_move == "scissors"
     @outcome = "We tied"
